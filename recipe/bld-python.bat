6985f7a0ff368fd191f4924619e46895e6e6a9a5
Nicolas Elie <40382614+n-elie@users.noreply.github.com>
2024-03-05 09:53:56 +0100

    Remove pin for qt6-main (#14)
    * Remove pin for qt6-main
    
    * Bump MACOSX_SDK_VERSION
    
    * MNT: Re-rendered with conda-build 24.1.2, conda-smithy 3.31.1, and conda-forge-pinning 2024.02.26.11.01.00
    
    * Set Python_EXECUTABLE instead of Python3_EXECUTABLE
    
    * Set both Python_EXECUTABLE and Python3_EXECUTABLE
    
    * Update hash
    
    * Add patch for setup.py to set python path
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld-python.bat
+++ recipe/bld-python.bat
@@ -1,6 +1,4 @@
 %PYTHON% -m pip install . ^
-    --config-settings="--global-option=build_ext" ^
-    --config-settings="--global-option=-DPython3_EXECUTABLE:FILEPATH=%PYTHON%" ^
     --no-build-isolation ^
     --no-deps ^
     --ignore-installed ^
